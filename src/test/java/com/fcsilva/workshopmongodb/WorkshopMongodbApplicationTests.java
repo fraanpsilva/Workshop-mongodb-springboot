6ccf1eb0ae5d9e3399e1634bd5248101b3140a68
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-04-05 00:53:31 +0100

    create project

---
+++ src/test/java/com/fcsilva/workshopmongodb/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcsilva.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
